8f2ceb96c916bdc7a28157557692d5f0f4778fa7
Dominik <wilk.dom132@gmail.com>
2023-05-14 12:03:11 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = '6bf3adc0-8c0c-49f1-aa7e-b5fef8bbc290';
+
+export const MODAL_CLOSE_SEC = 2.5;
